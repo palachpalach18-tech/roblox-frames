475d5f01f0e6592410a23cc0a73ced34a12de56f
palachpalach18-tech <palachpalach18@gmail.com>
2026-08-18 05:50:27 -0400

    Add 200 frames + FrameViewer.lua

--- FrameViewer.lua
+++ FrameViewer.lua
@@ -7,8 +7,8 @@ local playerGui = player:WaitForChild("PlayerGui")
 
 local GITHUB_BASE_URL = "https://raw.githubusercontent.com/USER/roblox-frames/main/frames"
 local LOCAL_DIR = "frames"
-local FPS = 29
-local FRAME_COUNT = 100
+local FPS = 20
+local FRAME_COUNT = 200
 local LAYERS = 5
 local SHOW_FPS_COUNTER = true
 local SHOW_DEBUG_PANEL = true
